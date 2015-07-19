622dd27b02aadbad5106187e05c585cb1154925b
monikkaelyse <monikkaelyse@gmail.com>
2015-07-18 20:03:11 -0700

    refactored html gen

--- html_generator.py
+++ html_generator.py
@@ -1,28 +1,30 @@
 Class HTMLGen:
+  ''' HTML code generator. '''
+
+  def open_bracket(self, html_tag):
+      return '<{}>'.format(html_tag)
+  def close_bracket(self, html_tag):
+      return '</{}>'.format(html_tag)
+  def generate_html(self, to_html, html_tag):
+      return '{0}{1}{2}'.format(self.open_bracket(html_tag), to_html, self.close_bracket(html_tag))
+      
+  def a(self, to_html):
+      return self.generate_html(to_html, 'a')
+  def b(self, to_html):
+      return self.generate_html(to_html, 'b')    
+  def p(self, to_html):
+      return self.generate_html(to_html, 'p')
+  def body(self, to_html):
+      return self.generate_html(to_html, 'body')
+  def div(self, to_html):
+      return self.generate_html(to_html, 'div')    
+  def span(self, to_html):
+      return self.generate_html(to_html, 'span')
+  def title(self, to_html):
+      return self.generate_html(to_html, 'title')    
+  def comment(self, to_html):
+      return '<!--{}-->'.format(to_html)lass HTMLGen:
   ''' Returns html generated code.'''
   pass
   
-  def a(self, str_to_html):
-      return '<a>' + str_to_html + '</a>'
-      
-  def b(self, str_to_html):
-      return '<b>' + str_to_html + '</b>'
-      
-  def p(self, str_to_html):
-      return '<p>' + str_to_html + '</p>'
-      
-  def body(self, str_to_html):
-      return '<body>' + str_to_html + '</body>'
-      
-  def div(self, str_to_html):
-      return '<div>' + str_to_html + '</div>'
-      
-  def span(self, str_to_html):
-      return '<span>' + str_to_html + '</span>'
-      
-  def title(self, str_to_html):
-      return '<title>' + str_to_html + '</title>'
-      
-  def comment(self, str_to_html):
-      return '<!--' + str_to_html + '-->'
             
